1004befdd8b60897e0414f1b603466b17d38bb72
xhoskin <xhoskin@gmail.com>
2014-03-16 20:08:35 +0600

    создание существа через прототип

--- js/script.js
+++ js/script.js
@@ -1,15 +1,7 @@
 $(function(){ 
 
+  //класс существа
   function Creature( name, level, city, damageMin, damageMax, attack, defense, health, speed, growth, price ){
-    //methods
-    this.print = function(index) {
-      var table = $('[data-creature="' + index + '"]');
-      for ( var key in this ) {
-        $(table).find('[data-value="' + key + '"]').text( this[key] );
-      }
-      $(table).find('[data-value="damage"]').text( this.damageMin + "-" + this.damageMax );
-    };
-
     //properties
     this.name      = name,
     this.city      = city,
@@ -22,26 +14,30 @@ $(function(){
     this.speed     = speed,
     this.growth    = growth,
     this.price     = price
-
   }
 
+  //методы существа
+  Creature.prototype.print = function(index) {
+    var table = $('[data-creature="' + index + '"]');
+    for ( var key in this ) {
+      $(table).find('[data-value="' + key + '"]').text( this[key] );
+    }
+    $(table).find('[data-value="damage"]').text( this.damageMin + "-" + this.damageMax );
+  };
+
+  //список существ и функция добавления в него новых существ
   var creaturesList = [];
   var newCreature = function( name, level, city, damageMin, damageMax, attack, defense, health, speed, growth, price ){
     creaturesList.push( new Creature( name, level, city, damageMin, damageMax, attack, defense, health, speed, growth, price ) );
   }
 
-  //creation
-  newCreature( "Копейщик", 1, "Замок", 1, 3, 4, 5, 10, 4, 14, 60 );
-  newCreature( "Алебардщик",	1,	"Замок",	2,	3,	6,	5,	10,	5,	14,	75 );
+  //создание существ
+  newCreature( "Копейщик",   1, "Замок", 1, 3, 4, 5, 10, 4, 14, 60 );
+  newCreature( "Алебардщик", 1, "Замок", 2,	3, 6, 5, 10, 5, 14, 75 );
 
-  //printing
+  //вывод всех существ на экран
   for (var i = 0; i < creaturesList.length; i++) {
     creaturesList[i].print(i);
   }
 
 });
-
-
-    apple  = red
-    grass += green
-    sky   -= blue
