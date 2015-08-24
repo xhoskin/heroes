44e9b17f516cc3500ab23606c5333d71570e37c8
xhoskin <xhoskin@gmail.com>
2015-08-24 10:26:35 +0600

    MWC

---
+++ js/controller.js
@@ -0,0 +1,17 @@
+var cities = new CityList();
+
+var calculate = new Calculate();
+var battle =  new Battlefield();
+
+$(function () {
+  cities.print()
+  $('#player-cities>li:nth(0)>a').click();
+  $('#player-creatures>li:nth(6)>a').click();
+  $('#player-creature-number').val(1);
+
+  $('#enemy-cities>li:nth(0)>a').click();
+  $('#enemy-creatures>li:nth(13)>a').click();
+  $('#enemy-creature-number').val(2);
+
+  battle.refresh();
+});
