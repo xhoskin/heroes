80b88e6b379120ae95835b7c88b3655270caab92
xhoskin <xhoskin@gmail.com>
2015-08-27 22:10:04 +0600

    переключение монстров

--- js/controllers.js
+++ js/controllers.js
@@ -12,8 +12,27 @@ heroesApp.controller('monstersListCtrl', ['$scope', '$http', function($scope, $h
         $scope.cities = data;
     });
 
+    // ** конструктор Стороны Битвы
+    $scope.BattleSide = function(monster){
+        return {
+            monster:    monster,
+            damage:     function(){
+                if ( this.monster.damageMin === this.monster.damageMax ) {
+                    return this.monster.damageMin
+                } else {
+                    return this.monster.damageMin + "-" + this.monster.damageMax;
+                }
+            },
+            setMonster: function(monster) {
+                this.monster = monster;
+            }
+        }
+    }
+
     $http.get('data/monsters.json').success(function(data, status, headers, config){
         $scope.monsters = data;
+        $scope.player = $scope.BattleSide(data[0]);
+        $scope.enemy = $scope.BattleSide(data[0]);
     });
 
 }]);
